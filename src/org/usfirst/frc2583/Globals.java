0699d85baf21949b0ea7767d4b3d69dc484675e2
Austin <amreuland@gmail.com>
2013-10-29 20:29:11 -0500

    Getting ready for the 2014 Robot

---
+++ src/org/usfirst/frc2583/Globals.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc2583;
+
+public class Globals {
+	
+	public static double joy1Axis[];
+	public static boolean joy1Btns[];
+	
+	public static double joy2Axis[];
+	public static boolean joy2Btns[];
+	
+	public Globals(){
+		
+	}
+
+}
---
+++ src/org/usfirst/frc2583/OI.java
@@ -0,0 +1,7 @@
+
+package org.usfirst.frc2583;
+
+public class OI {
+    // Process operator interface input here.
+}
+
