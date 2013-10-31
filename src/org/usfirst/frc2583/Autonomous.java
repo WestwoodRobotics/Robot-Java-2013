b692b53fc6866f565ff0894c059e6527c8008223
Austin <amreuland@gmail.com>
2013-10-30 22:32:48 -0500

    Added AUtonomous and Teleop Classes

---
+++ src/org/usfirst/frc2583/Autonomous.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc2583;
+
+public class Autonomous {
+	
+	public int m_loops;
+	
+	public Autonomous(){
+		m_loops = 0;
+		
+	}
+	
+	public void run(){
+		m_loops++;
+	}
+
+}
---
+++ src/org/usfirst/frc2583/Teleop.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc2583;
+
+public class Teleop {
+	
+	public int m_loops;
+	
+	public Teleop(){
+		m_loops = 0;
+	}
+	
+	public void run(){
+		m_loops++;
+		double motor_l = Globals.m_joy1.getRawAxis(0);
+		double motor_r = Globals.m_joy1.getRawAxis(1);
+		int sign_l = Globals.sign(motor_l);
+		int sign_r = Globals.sign(motor_r);
+		motor_l = Math.pow(motor_l, 2) * sign_l;
+		motor_r = Math.pow(motor_r, 2) * sign_r;
+		Globals.m_driveMain.tankDrive(Globals.m_joy1, 0, Globals.m_joy1, 1);
+				
+	}
+
+}
